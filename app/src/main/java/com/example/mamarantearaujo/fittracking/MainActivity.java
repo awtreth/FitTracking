1e93333b5f9611b724bfd94cc99a445d5738f493
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2016-03-09 21:12:00 -0500

    Current Location is working

--- app/src/main/java/com/example/mamarantearaujo/fittracking/MainActivity.java
+++ app/src/main/java/com/example/mamarantearaujo/fittracking/MainActivity.java
@@ -23,15 +23,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class MainActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleApiClient mApiClient = null;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buildApiClient();
+        Log.v(TAG, "created");
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
@@ -61,4 +69,37 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.Co
 
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        enableMyLocation();
+    }
+
+    private void enableMyLocation() {
+        Log.v(TAG, "Entered");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            //TODO: print an error
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            Log.v(TAG, "enabled");
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    //TODO: print an error
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+    }
 }
