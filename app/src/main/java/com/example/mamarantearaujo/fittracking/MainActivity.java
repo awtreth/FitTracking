9aff81acc9ef3bfca8a20c6538d7a439bdee4d29
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2016-03-13 01:14:28 -0500

    Added some comments and updated ActivityIds (created a map)

--- app/src/main/java/com/example/mamarantearaujo/fittracking/MainActivity.java
+++ app/src/main/java/com/example/mamarantearaujo/fittracking/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.mamarantearaujo.fittracking;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
@@ -23,38 +17,65 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.DetectedActivity;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+/*
+    UI Activity. It handles the connection with the Activitity Recognition Google Service and view updates
+ */
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private GoogleApiClient mApiClient = null;
-    private final String TAG = "Main";
-    public static Handler mHandler;
+    private GoogleApiClient mApiClient = null;//To connect to Google Servieces
+    private final String TAG = "Main";//for debug purposes
+    public static Handler mHandler;//Global handler to be used in ActivityRecognizedService
 
-    private int mLastActivity = DetectedActivity.STILL;
-    private long mLastTime = 0;
+    //Store the lastActivity (actually it's the current while there is no activity update)
+    private int mLastActivity = DetectedActivity.STILL;//STILL by default (only for the 1st time)
+    private long mLastTime = 0;//Store the time stamp of mLastActivity
 
     private ImageView mImageView;
     private TextView mTextView;
 
+
+    /*
+    Auxiliar class that store Ids related to each activity
+     */
+    private class ActivityIds {
+        public int imageId;
+        public int textViewId;
+        public int toastMsgId;
+
+        ActivityIds(int imageId, int textViewId, int toastMsgId) {
+            this.imageId = imageId;
+            this.textViewId = textViewId;
+            this.toastMsgId = toastMsgId;
+        }
+    }
+
+    private HashMap<Integer, ActivityIds> mActivityIds;//Map each activity to its ActivityIds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buildApiClient();
+        buildApiClient();//Initialize mApiClient
 
         mTextView = (TextView) findViewById(R.id.textView);
         mImageView = (ImageView) findViewById(R.id.imageView);
 
+        //Create Hashmap
+        mActivityIds = new HashMap<Integer, ActivityIds>();
+        mActivityIds.put(DetectedActivity.STILL, new ActivityIds(R.drawable.still, R.string.still, R.string.still));
+        mActivityIds.put(DetectedActivity.WALKING, new ActivityIds(R.drawable.walking, R.string.walking, R.string.walked));
+        mActivityIds.put(DetectedActivity.RUNNING, new ActivityIds(R.drawable.running, R.string.running, R.string.run));
+        mActivityIds.put(DetectedActivity.IN_VEHICLE, new ActivityIds(R.drawable.in_vehicle, R.string.in_vehicle, R.string.in_vehicle));
+
+
+        //In case of landscape-portrait switch
         if (savedInstanceState != null) {
             mLastActivity = savedInstanceState.getInt("Activity");
             mLastTime = savedInstanceState.getLong("Time");
@@ -62,15 +83,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         updateView(mLastActivity);
 
+        //Initialize global handler (it expects updates from ActivityRecognizedService)
         mHandler = new Handler(Looper.getMainLooper()) {
             @Override
-            public void handleMessage(Message msg) {
+            public void handleMessage(Message msg) {//Handle the messages sent by ActivityRecognizedService
+                //Detected Activity and Time
                 int activity = msg.getData().getInt("ActivityType");
                 long time = msg.getData().getLong("ActivityTime");
+
                 Log.v(TAG, String.format("received msg %d", activity));
+
+                //The activity has changed OR it's the first run
                 if (activity != mLastActivity || mLastTime == 0) {
-                    if (mLastTime != 0)
+                    if (mLastTime != 0)//not the first run
                         toastMsg(mLastActivity, time - mLastTime);
+
                     updateView(activity);
                     mLastActivity = activity;
                     mLastTime = time;
@@ -86,43 +113,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onSaveInstanceState(outState);
     }
 
-    private class ActivityIds {
-        public int imageId;
-        public int textViewId;
-        public int toastMsgId;
-
-        ActivityIds(int activity) {
-            switch (activity) {
-                case DetectedActivity.STILL:
-                    textViewId = R.string.still;
-                    imageId = R.drawable.still;
-                    toastMsgId = R.string.still;
-                    break;
-                case DetectedActivity.WALKING:
-                    textViewId = R.string.walking;
-                    toastMsgId = R.string.walked;
-                    imageId = R.drawable.walking;
-                    break;
-                case DetectedActivity.IN_VEHICLE:
-                    textViewId = R.string.in_vehicle;
-                    toastMsgId = R.string.in_vehicle;
-                    imageId = R.drawable.in_vehicle;
-                    break;
-                case DetectedActivity.RUNNING:
-                    textViewId = R.string.running;
-                    toastMsgId = R.string.run;
-                    imageId = R.drawable.running;
-                    break;
-                default:
-                    Log.v("ActivityId", "NOT SUPPOSED TO BE HERE");
-            }
-        }
-    }
-
+    /*
+    Auxiliary method
+    Input in milliseconds
+    Output: "[x hour(s)] [y minute(s)] z second(s)"
+     */
     private String timeToString(long time) {
         String str = new String(" ");
 
-        time = time / 1000;
+        time = time / 1000;//milliseconds to seconds
         Integer hours = (int) (time / 3600);
         Integer minutes = (int) ((time % 3600) / 60);
         Integer seconds = (int) time % 60;
@@ -135,7 +134,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             str = str + " " + minutes.toString() + " ";
             str = str + getResources().getQuantityString(R.plurals.minute, minutes);
         }
-
         if (seconds > 0) {
             str = str + " " + seconds.toString() + " ";
             str = str + getResources().getQuantityString(R.plurals.second, seconds);
@@ -146,23 +144,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
 
     void toastMsg(int activity, long time) {
-        ActivityIds activityIds = new ActivityIds(activity);
+        ActivityIds activityIds = mActivityIds.get(activity);//new ActivityIds(activity);
         Toast.makeText(this, String.format(getString(R.string.toast_msg), getString(activityIds.toastMsgId), timeToString(time)), Toast.LENGTH_SHORT).show();
     }
 
     void updateView(int activity) {
-        ActivityIds activityIds = new ActivityIds(activity);
+        ActivityIds activityIds = mActivityIds.get(activity);//new ActivityIds(activity);
         mTextView.setText(String.format(getString(R.string.text_msg), getString(activityIds.textViewId)));
         mImageView.setImageResource(activityIds.imageId);
 
     }
 
     // Communication with the Google Service ActivityRecognitionApi
-    //TODO: complete
     @Override
     protected void onStart() {
-        super.onStart();
         mApiClient.connect();
+        super.onStart();
     }
 
     @Override
